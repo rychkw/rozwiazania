4aa40a96f281a2637f361e485f272a23ffca6a5b
kwiasylw <sylwia_664@interia.pl>
2018-02-28 16:30:30 +0100

    .

--- lab1/factorial/Factorial.cpp
+++ lab1/factorial/Factorial.cpp
@@ -4,5 +4,16 @@
 #include "Factorial.h"
 
 int factorial(int value) {
-  return 0;
+  if(value<0){
+    return value*factorial(value+1);
+  }
+    else if(value==0){
+      return 1;
+    }
+      else if (value>12){
+      return 0;
+  }
+  else{
+      return value*factorial(value-1);
+  }
 }
